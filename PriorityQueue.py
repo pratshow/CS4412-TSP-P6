40b3667610bf0a6f28eff7f08ba6d64d9ecdc6ca
pratshow <pratshow@isu.edu>
2022-04-19 11:17:30 -0600

    inital commit

---
+++ PriorityQueue.py
@@ -0,0 +1,263 @@
+# Brandon Watkins
+# CS 4412
+
+import math
+
+
+class PrioritizedNode:
+
+    def __init__(self, element, priority = math.inf):
+        self.priority = priority
+        self.element = element
+
+    def delete(self):
+        element = self.element
+        self.__delattr__("priority")
+        self.__delattr__("element")
+        return element
+
+
+class PrioritizedHeapNode(PrioritizedNode):
+
+    def __init__(self, element, priority = math.inf):
+        super().__init__(element, priority)
+        self.parent = self
+        self.children = []
+
+    # For garbage collection
+    def delete(self):
+        element = self.element
+        self.__delattr__("parent")
+        self.__delattr__("priority")
+        self.__delattr__("children")
+        self.__delattr__("element")
+        return element
+
+    def __hash__(self):
+        try:
+            return hash(self.element)
+        except:
+            if str(self.element.toString()):
+                return hash(str(self.element.toString()))
+
+
+class PriorityQueue:
+
+    def __init__(self):
+        self.queue = []
+
+    def makeQueue(self, elements, defaultPriority):
+        pass
+
+    def insert(self, element, priority):
+        pass
+
+    def deleteMin(self):
+        pass
+
+    def decreaseKey(self, element):
+        pass
+
+    def changePriority(self, element, priority):
+        pass
+
+    def notEmpty(self):
+        return self.queue is not None and len(self.queue) > 0
+
+
+class DAryHeapPriorityQueue(PriorityQueue):
+
+    def __init__(self, d = 3, elementList = []):
+        super().__init__()
+        # d is the number of children a node can have.
+        self.d = d
+        self.dic = dict()
+        self.queue = []
+        if len(elementList) > 0:
+            self.makeQueue(elementList)
+
+    # Creates a priority queue with all priority levels the same
+    def makeQueue(self, elements, defaultPriority = math.inf):
+        for element in elements:
+            newNode = PrioritizedHeapNode(element, defaultPriority)
+            if len(self.queue) > 0:
+                newNode.parent = self.queue[math.ceil(len(self.queue) / self.d) - 1]
+                newNode.parent.children.append(newNode)
+            self.queue.append(newNode)
+            self.dic[hash(newNode)] = len(self.queue) - 1
+        return self
+
+    # Insert a new node into the priority queue
+    def insert(self, element, priority = math.inf):
+        # Create a new node
+        newNode = PrioritizedHeapNode(element, priority)
+
+        # Add parent node
+        if len(self.queue) > 0:
+            newNode.parent = self.queue[math.floor(len(self.queue) / self.d)]
+            newNode.parent.children.append(newNode)
+
+        # Place node at the end of the queue.
+        self.queue.append(newNode)
+
+        # Place the node pointer in the dictionary
+        self.dic[hash(newNode)] = len(self.queue) - 1
+
+        # Bubble up the new node
+        self.bubbleUp(newNode)
+
+        return newNode
+
+    # Removes the root, and returns its element
+    def deleteMin(self):
+        if len(self.queue) < 1:
+            return
+
+        # Store the root node in temp.
+        root = self.queue[0]
+
+        # Swap the last node with the root
+        if len(self.queue) > 1:
+            self.swap(self.queue[0], self.queue[len(self.queue) - 1])
+
+        # Remove the root from its new parent
+        if root in root.parent.children:
+            root.parent.children.remove(root)
+
+        # Remove the node from the dictionary
+        self.dic.pop(hash(root))
+
+        # Remove the node from the queue
+        self.queue.__delitem__(len(self.queue) - 1)
+
+        # Pulls out the node's element
+        temp = root.delete()
+
+        # Let the new root bubble back down
+        if self.notEmpty():
+            self.bubbleDown(self.queue[0])
+
+        # Returns the element stored inside the old root
+        return temp
+
+    # Verifies that the node is in the correct position in the priority queue. Use after lowering its priority.
+    def decreaseKey(self, modifiedNode):
+        self.bubbleUp(modifiedNode)
+        return modifiedNode
+
+    # Updates the priority of the node
+    def changePriority(self, nodeToModify, priority):
+        oldPriority = nodeToModify.priority
+        nodeToModify.priority = priority
+        if oldPriority > priority:
+            self.decreaseKey(nodeToModify)
+        else:
+            self.bubbleDown(nodeToModify)
+        return nodeToModify
+
+    # Swaps two nodes.
+    def swap(self, node1, node2):
+        if type(node1) == int:
+            node1 = self.queue[node1]
+        if type(node2) == int:
+            node2 = self.queue[node2]
+        n1Index = self.dic[hash(node1)]
+        n2Index = self.dic[hash(node2)]
+        node1Parent = node1.parent
+        node2Parent = node2.parent
+
+        # Swap the nodes in the queue array
+        self.queue[n1Index], self.queue[n2Index] = self.queue[n2Index], self.queue[n1Index]
+
+        # Update the node pointers in the dictionary
+        self.dic[hash(node1)] = n2Index
+        self.dic[hash(node2)] = n1Index
+
+        # Swap the nodes' children
+        node1.children, node2.children = node2.children, node1.children
+
+        # Assign the nodes' parents
+        # If node2 is node1's parent:
+        if node1.parent == node2:
+            # If node2 is the root:
+            if node2.parent == node2:
+                node1.parent = node1
+            # Else node2 is mid-tree
+            else:
+                node1.parent = node2.parent
+            node2.parent = node1
+
+        # If node1 is node2's parent:
+        else:
+            if node2.parent == node1:
+                # If node1 is the root:
+                if node1.parent == node1:
+                    node2.parent = node2
+                # Else node1 is mid-tree
+                else:
+                    node2.parent = node1.parent
+                node1.parent = node2
+
+            # If neither node is the other's parent:
+            else:
+                # If node1 is the root:
+                if node1.parent == node1:
+                    node1.parent = node2.parent
+                    node2.parent = node2
+                # If node2 is the root:
+                else:
+                    if n2Index == 0:
+                        node2.parent = node1.parent
+                        node1.parent = node1
+                    # If neither node is the parent of the other, and neither is the root:
+                    else:
+                        node1.parent, node2.parent = node2.parent, node1.parent
+
+        # Add the nodes to their new parent's children list
+        if node1.parent != node1:
+            node1.parent.children.append(node1)
+        if node2.parent != node2:
+            node2.parent.children.append(node2)
+
+        # Remove the nodes from old parent's children list
+        if node2Parent != node1 and node2 in node2Parent.children:
+            node2Parent.children.remove(node2)
+        if node1Parent != node2 and node1 in node1Parent.children:
+            node1Parent.children.remove(node1)
+
+        # Remove the nodes if they were added to their own children during the children swap:
+        if node1 in node1.children:
+            node1.children.remove(node1)
+        if node2 in node2.children:
+            node2.children.remove(node2)
+
+        # Reassign the nodes to be their new children's parents
+        for c in node1.children:
+            c.parent = node1
+        for c in node2.children:
+            c.parent = node2
+
+    # Moves the node down the priority queue, further from the root, while its children have lower edge weights/priority
+    def bubbleDown(self, nodeToSettle):
+        # Swap node and its child if its child has lower priority (lower edge weight)
+        childToSwap = None
+        for c in nodeToSettle.children:
+            if nodeToSettle.priority > c.priority:
+                if childToSwap is None or childToSwap.priority > c.priority:
+                    childToSwap = c
+        if childToSwap is not None:
+            self.swap(nodeToSettle, childToSwap)
+            self.bubbleDown(nodeToSettle)
+        return nodeToSettle
+
+    # Moves the node up the priority queue, closer to the root, while its parent has a higher edge weight/priority
+    def bubbleUp(self, nodeToSettle):
+        # Swap node and its child if its child has lower priority (lower edge weight)
+        if nodeToSettle.parent.priority > nodeToSettle.priority:
+            self.swap(nodeToSettle, nodeToSettle.parent)
+            self.bubbleUp(nodeToSettle)
+        return nodeToSettle
+
+    def notEmpty(self):
+        return super().notEmpty()
+
---
+++ TSPSolver.py
@@ -0,0 +1,396 @@
+# Brandon Watkins
+# CS 4412
+
+#!/usr/bin/python3
+import math
+
+from PriorityQueue import *
+from which_pyqt import PYQT_VER
+if PYQT_VER == 'PYQT5':
+	from PyQt5.QtCore import QLineF, QPointF
+elif PYQT_VER == 'PYQT4':
+	from PyQt4.QtCore import QLineF, QPointF
+else:
+	raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
+
+
+
+
+import time
+import numpy as np
+from TSPClasses import *
+import heapq
+import itertools
+from copy import deepcopy
+
+
+
+class TSPSolver:
+	def __init__( self, gui_view ):
+		self._scenario = None
+		self._bssf = None
+
+	def setupWithScenario( self, scenario ):
+		self._scenario = scenario
+
+
+	''' <summary>
+		This is the entry point for the default solver
+		which just finds a valid random tour.  Note this could be used to find your
+		initial BSSF.
+		</summary>
+		<returns>results dictionary for GUI that contains three ints: cost of solution, 
+		time spent to find solution, number of permutations tried during search, the 
+		solution found, and three null values for fields not used for this 
+		algorithm</returns> 
+	'''
+	
+	def defaultRandomTour( self, time_allowance=60.0 ):
+		results = {}
+		cities = self._scenario.getCities()
+		ncities = len(cities)
+		foundTour = False
+		count = 0
+		bssf = None
+		start_time = time.time()
+		while not foundTour and time.time()-start_time < time_allowance:
+			# create a random permutation
+			perm = np.random.permutation( ncities )
+			route = []
+			# Now build the route using the random permutation
+			for i in range( ncities ):
+				route.append( cities[ perm[i] ] )
+			bssf = TSPSolution(route)
+			count += 1
+			if bssf.cost < np.inf:
+				# Found a valid route
+				foundTour = True
+		end_time = time.time()
+		results['cost'] = bssf.cost if foundTour else math.inf
+		results['time'] = end_time - start_time
+		results['count'] = count
+		results['soln'] = bssf
+		results['max'] = None
+		results['total'] = None
+		results['pruned'] = None
+		return results
+
+
+	''' <summary>
+		This is the entry point for the greedy solver, which you must implement for 
+		the group project (but it is probably a good idea to just do it for the branch-and
+		bound project as a way to get your feet wet).  Note this could be used to find your
+		initial BSSF.
+		</summary>
+		<returns>results dictionary for GUI that contains three ints: cost of best solution, 
+		time spent to find best solution, total number of solutions found, the best
+		solution found, and three null values for fields not used for this 
+		algorithm</returns> 
+	'''
+
+	def greedy(self, time_allowance=60.0):
+		pass
+
+	
+	''' <summary>
+		This is the entry point for the branch-and-bound algorithm that you will implement
+		</summary>
+		<returns>results dictionary for GUI that contains three ints: cost of best solution, 
+		time spent to find best solution, total number solutions found during search (does
+		not include the initial BSSF), the best solution found, and three more ints: 
+		max queue size, total number of states created, and number of pruned states.</returns> 
+	'''
+
+	def branchAndBound(self, time_allowance=60.0):
+		results = {}
+		cities = self._scenario.getCities()
+		n_cities = len(cities)
+		foundTour = False
+		intermediateSolutions = 0
+		totalPruned = 0
+		maxQueueSize = 0
+		totalChildStatesGenerated = 0
+		start_time = time.time()
+
+		self._bssf = self.initialBSSF(cities=cities, attempts=n_cities ** 2, time_allowance=time_allowance / 5)
+
+		baseProblem = State(partialPath=cities)
+		S = DAryHeapPriorityQueue(d=n_cities, elementList=[baseProblem])
+		while S.notEmpty() and time.time() - start_time < time_allowance:
+			maxQueueSize = max(maxQueueSize, len(S.queue))
+			subproblem = S.deleteMin()
+			if subproblem.lowerbound() == math.inf or (self._bssf is not None and subproblem.lowerbound() > self._bssf.lowerbound()):
+				totalPruned += 1
+				continue
+			smallerSubproblems = subproblem.expand()
+			totalChildStatesGenerated += len(smallerSubproblems)
+
+			# Unsure if I'm supposed to prune cities inside expand(). Also unsure if I'm supposed to include them in totalPruned
+			totalPruned += (n_cities - len(subproblem.partialPath) - len(smallerSubproblems))
+
+			for P in smallerSubproblems:
+				if P.lowerbound() == math.inf: totalPruned += 1
+				elif P.isValidSolution():
+					if self._bssf is None or P.lowerbound() < self._bssf.lowerbound():
+						self._bssf = P
+						intermediateSolutions += 1
+						foundTour = True
+				elif self._bssf is None or P.lowerbound() < self._bssf.lowerbound():
+					# Scales the priority, so that initially it favors a "deep" search, gradually transitioning to wide
+					#S.insert(P, P.lowerbound() * (time_allowance - (time.time() - start_time)) / (P.partialPathSize() * time_allowance))
+					S.insert(P, P.lowerbound() * time_allowance / (P.partialPathSize() * (time_allowance - (time.time() - start_time))))
+				else:
+					totalPruned += 1
+
+		maxQueueSize = max(maxQueueSize, len(S.queue))
+		totalPruned += len(S.queue)
+		end_time = time.time()
+
+		self._bssf = TSPSolution(getCityPath(cities, self._bssf)) if self._bssf is not None else None
+
+		results['cost'] = self._bssf.cost if foundTour else math.inf
+		results['time'] = end_time - start_time
+		results['count'] = intermediateSolutions
+		results['soln'] = self._bssf if foundTour else None
+		results['max'] = maxQueueSize
+		results['total'] = totalChildStatesGenerated
+		results['pruned'] = totalPruned
+
+		return results
+
+
+	''' <summary>
+		This is the entry point for the algorithm you'll write for your group project.
+		</summary>
+		<returns>results dictionary for GUI that contains three ints: cost of best solution, 
+		time spent to find best solution, total number of solutions found during search, the 
+		best solution found.  You may use the other three field however you like.
+		algorithm</returns> 
+	'''
+		
+	def fancy(self, time_allowance=60.0):
+		pass
+
+
+	'''
+		Attempts to find an initial BSSF via an incredibly simple greedy algorithm, followed by checking random routes
+		for the given number of attempts. Returns the best route's state, or None if none found.
+	'''
+
+	def initialBSSF(self, cities, attempts = 3, time_allowance = 10.):
+		bssf = None
+		start_time = time.time()
+
+		# Just checking for a quick greedy route
+		base = State(partialPath=cities)
+		while len(base.partialPath) < len(cities):
+			children = base.expand()
+			if len(children) == 0: break
+			children.sort(key=lambda c: c.bound)
+			base = children[0]
+		if base.isValidSolution():
+			bssf = base
+
+		# Checking random routes for the specified number of attempts
+		count = 0
+		indices = [i for i in range(1, len(cities))]
+		while time.time()-start_time < time_allowance and count < attempts:
+			base = State(partialPath=cities)
+			random.shuffle(indices)
+
+			for j in range(len(indices)):
+				if base.lowerbound() == math.inf or base.isValidSolution(): break
+				children = base.expand()
+				if len(children) == 0: break
+				for child in children:
+					if child.id == indices[j]:
+						base = child
+						break
+
+			if base.isValidSolution():
+				if bssf is None or base.lowerbound() < bssf.lowerbound(): bssf = base
+			count += 1
+
+		# Ran out of time, or attempts. Returning best solution, or None if none found
+		return bssf
+
+
+'''
+	Computes the new lower bound for the given matrix, and reduces the cost matrix.
+	Modifies the input State's costMatrix and lowerBound, in place.
+'''
+
+def lowerBound(state):
+	assert(type(state) == State)
+	# Reduce cost matrix rows
+	# This represents taking the cheapest edge FROM each of the nodes.
+	# Reducing the other values by this amount, because taking those edges instead would result in their cell's cost,
+	# in addition to the cheapest.
+	for i in range(len(state.costMatrix[0])):
+		cheapest = min(state.costMatrix[i])
+		if cheapest == math.inf:
+			if not state.partialPath.__contains__(i): state.bound = math.inf
+			continue
+		state.bound += cheapest
+		for j in range(len(state.costMatrix[i])):
+			state.costMatrix[i][j] -= cheapest
+
+	# Reduce cost matrix columns
+	# This represents taking the cheapest edge TO each of the nodes.
+	for j in range(len(state.costMatrix)):
+		cheapest = min(row[j] for row in state.costMatrix)
+		if cheapest == math.inf:
+			if not state.partialPath.__contains__(j): state.bound = math.inf
+			continue
+		state.bound += cheapest
+		for i in range(len(state.costMatrix[j])):
+			state.costMatrix[i][j] -= cheapest
+
+
+'''
+	Computes/stores the cost to go from every node to every other node, and stores costs in a matrix.
+	Note: This does not reduce the matrix at all.
+'''
+
+def initialCostMatrix(cities):
+	# Create initial cost matrix
+	costMatrix = []
+	for i in range(len(cities)):
+		inner = []
+		for j in range(len(cities)):
+			inner.append(math.inf)
+		costMatrix.append(inner)
+
+	# Fill out cost matrix
+	i = 0
+	j = 0
+	for city in cities:
+		for innerCity in cities:
+			if city != innerCity:
+				costMatrix[i][j] = city.costTo(innerCity)
+			j += 1
+		j = 0
+		i += 1
+
+	return costMatrix
+
+
+'''
+	This is what "visits" the next node, adjusting the bound, and adding the new math.inf row/col.
+	The cost matrix still needs to be reduced.
+'''
+
+def updateCostMatrix(state = None):
+	assert type(state) == State
+	state.bound += state.costMatrix[state.partialPath[-2]][state.id]
+
+	for i in range(len(state.costMatrix[0])):
+		state.costMatrix[state.partialPath[-2]][i] = math.inf
+		state.costMatrix[i][state.id] = math.inf
+	state.costMatrix[state.id][state.partialPath[-2]] = math.inf
+
+	# Was used before I fixed my partialPath
+	#assert type(state) == State
+	#state.bound += state.costMatrix[state.parent.id][state.id]
+
+	#for i in range(len(state.costMatrix[0])):
+	#	state.costMatrix[state.parent.id][i] = math.inf
+	#	state.costMatrix[i][state.id] = math.inf
+	#state.costMatrix[state.id][state.parent.id] = math.inf
+
+
+'''
+	Gets the actual list of cities in order of "visitation" from the travelling salesman (starting city is first/index 0)
+'''
+
+def getCityPath(cities, state = None):
+	assert(type(state) == State)
+	path = state.getPath()
+	returnPath = []
+	for cityIndex in path:
+		returnPath.append(cities[cityIndex])
+	return returnPath
+
+
+'''
+	State for storing the cost matrix, lowerbound, and partial path (cities remaining to be visited).
+	Also includes some utility functions for extracting child states, automating calculations, and generating full path.
+	Note: partialPath originally contains the list of cities, but all children states use this to reference the 
+	city index that is about to be evaluated. self.partialPath will contain a list of all city indexes waiting to be evaluated.
+'''
+
+class State:
+
+	def __init__(self, parent = None, partialPath = None):
+		assert(partialPath is not None)
+		assert (parent is None or type(parent) == State)
+		assert((parent is not None and not parent.partialPath.__contains__(partialPath)) or parent is None)
+		#self.parent = parent # Was used before I fixed my partialPath
+		self.bound = parent.bound if parent is not None else 0
+		# To keep track of which city this is for.
+		self.id = partialPath if parent is not None else 0 # Can probably get rid of this, was used before I fixed my partialPath
+
+		if parent is None:
+			self.partialPath = [self.id]
+			self.costMatrix = initialCostMatrix(partialPath)
+		else:
+			self.costMatrix = deepcopy(parent.costMatrix)
+			self.partialPath = deepcopy(parent.partialPath)
+			self.partialPath.append(partialPath)
+			updateCostMatrix(self)
+
+		lowerBound(self)
+
+	def lowerbound(self):
+		return self.bound
+
+	def partialPathSize(self):
+		return len(self.partialPath)
+
+	def isCompleteSolution(self):
+		return len(self.partialPath) == len(self.costMatrix[0])
+
+	def isValidSolution(self):
+		return self.isCompleteSolution() and self.costMatrix[self.id][self.partialPath[0]] != math.inf and self.bound != math.inf
+
+		# Was used before I fixed my partialPath
+		#a = self
+		#while a.parent is not None:
+		#	a = a.parent
+		#return self.isCompleteSolution() and self.costMatrix[self.id][a.id] != math.inf and self.bound != math.inf
+
+	def expand(self):
+		children = []
+		for i in range(len(self.costMatrix[0])):
+			if self.costMatrix[self.id][i] != math.inf and (
+					not self.partialPath.__contains__(i) or
+					(self.partialPath.index(i) == len(self.partialPath) - 1)
+					and self.partialPath.count(i) == 1):
+				children.append(State(parent=self, partialPath=i))
+		return children
+
+	def getPath(self):
+		return self.partialPath
+
+		# Was used before I fixed my partialPath
+		#a = self
+		#cities = []
+		#while a is not None:
+		#	cities.insert(0, a.id)
+		#	a = a.parent
+		#return cities
+
+	def toString(self):
+		s = ""
+		for i in range(len(self.partialPath) - 1):
+			s += str(self.partialPath[i]) + ", "
+		return s + str(self.partialPath[len(self.partialPath) - 1])
+
+		# Was used before I fixed my partialPath
+		#a = self.parent
+		#s = str(self.id)
+		#while a is not None:
+		#	s = str(a.id) + "," + s
+		#	a = a.parent
+		#return s
+
